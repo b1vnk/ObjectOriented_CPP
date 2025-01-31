283dcd05d36d31ff680b4fbf53bdd25be0cd3153
dezmnfr <car1@cpp.edu>
2025-01-31 13:21:25 -0800

    Create hello.cpp

---
+++ Lab1/hello.cpp
@@ -0,0 +1,14 @@
+/*
+    Lab 1
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
